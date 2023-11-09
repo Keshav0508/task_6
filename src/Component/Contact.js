2a439c8b9a92b1fc7771e1e6dda7af7a2c239bc2
Keshav <gkeshav717@gmail.com>
2023-11-10 02:28:22 +0530

    first commit

---
+++ src/Component/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <section className="contact">
+            <h2>Contact Me</h2>
+            <p>Email: keshav.gupta2021@vitstudent.ac.in </p>
+            <p>Phone: 6392353823</p>
+        </section>
+    );
+}
+
+export default Contact;
---
+++ src/Component/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <section className="home">
+            <h1>Keshav Gupta</h1>
+            <h1>Welcome to My Portfolio</h1>
+            <p>Front-End Developer</p>
+        </section>
+    );
+}
+
+export default Home;
